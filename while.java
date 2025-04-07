17b80d19bd6e600463325c9df46e76d04a269934
Parth Shukla <2k23.psitaiml2310215@gmail.com>
2025-04-07 22:22:09 +0530

    Create while.java

---
+++ while.java
@@ -0,0 +1,15 @@
+public class While2 {
+    public static void main(String[] args){
+        int num=1;
+        while(num<=6){
+            if(num<6) {
+                System.out.println("Yatzy no.");
+            }else {
+                System.out.println("NO Yatzy no.");
+            }
+            num=num+1;
+
+        }
+
+    }
+}
