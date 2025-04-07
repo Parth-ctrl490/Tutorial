c9f674fb7119547c1415ff91e1e125e61703a6ef
Parth Shukla <2k23.psitaiml2310215@gmail.com>
2025-04-07 22:28:14 +0530

    Create functions.java

---
+++ functions.java
@@ -0,0 +1,38 @@
+/*public class Functions {
+    public static void myFunction(){
+        System.out.println("this is a function");
+    }
+    public static void main(String[] args){
+        myFunction();
+        myFunction();
+        myFunction();
+        myFunction();
+    }
+
+}*/
+
+//parameters
+/*public class Functions {
+    public static void myFunction(String names) {
+        System.out.println(names+"Shukla");
+    }
+    public static void main(String[] args){
+        myFunction("Parth");
+        myFunction("Lalit");
+        myFunction("neetu");
+        myFunction("ajit");
+    }
+    }*/
+
+//value
+public class Functions {
+    public int method(int x){
+       return 5+x;
+    }
+    public void main(String[] args){
+        System.out.println(method(1));
+        System.out.println(method(2));
+
+
+    }
+}
