0d32c85aab99e31def6655f53ac428057508ed35
Parth Shukla <2k23.psitaiml2310215@gmail.com>
2025-04-07 22:30:16 +0530

    Create attributes.java

---
+++ attributes.java
@@ -0,0 +1,20 @@
+//Accessing attributes using object
+//public class Attrubutes {
+//    int x=10;
+//    public static void main(String[] args){
+//        Attrubutes obj=new Attrubutes();
+//        System.out.println(obj.x);
+//    }
+//
+//}//10
+
+
+//modifying attributes
+public class Attrubutes {
+    int x=10;
+    public static void main(String[] args){
+        Attrubutes obj=new Attrubutes();
+        obj.x=24;
+        System.out.println(obj.x);
+    }
+}//modifies to 24
