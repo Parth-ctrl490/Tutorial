c21868a3abe83b9c21aeb1dcd08803941bbad245
Parth Shukla <2k23.psitaiml2310215@gmail.com>
2025-04-07 15:32:26 +0530

    Create dowhile.java

---
+++ dowhile.java
@@ -0,0 +1,12 @@
+//do while loop
+public class While {
+    public static void main(String[] args){
+        int count=5;
+        do{
+            System.out.println(count);
+            count--;
+        }
+        while(count>=0);
+        System.out.println("Happy new year");
+    }
+}
