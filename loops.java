8cd6913c5f0431561562000a0a6a251d8946560e
Parth Shukla <2k23.psitaiml2310215@gmail.com>
2025-04-07 22:23:30 +0530

    Create loops.java

---
+++ loops.java
@@ -0,0 +1,33 @@
+//nested for loop
+/*public class loops {
+    public static void main(String[] args){
+        for(int i=0;i<=2;i++){
+            System.out.println("Outer"+" "+i);
+            for(int j=0;j<=2;j++){
+                System.out.println("inner"+" "+j);
+        }}
+    }
+}*/
+
+
+//for each loop
+/*public class loops {
+    public static void main(String[] args) {
+    String[] names={"Parth","nikhil","sirjan","roshni"};
+    for(String n:names){
+        System.out.println("names are:"+ n);
+    }
+    }
+    }*/
+
+//table of 2
+public class loops {
+    public static void main(String[] args) {
+    int i;
+    for(i=1;i<=10;i++){
+        int num=2;
+        System.out.println("2"+"X"+i+"="+(num*i));
+    }
+    }
+
+    }
