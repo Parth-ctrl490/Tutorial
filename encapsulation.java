e46aecc97bbf5ed54f826e7d10c5267787dfde1d
Parth Shukla <2k23.psitaiml2310215@gmail.com>
2025-04-07 22:25:29 +0530

    Create encapsulation.java

---
+++ encapsulation.java
@@ -0,0 +1,11 @@
+public class Encapsulation {
+    private String name;
+    public String getname(){
+        return name;
+    }
+    public void setname(String newname){
+        this.name=newname;
+    }
+}
+
+//second part in main.java
