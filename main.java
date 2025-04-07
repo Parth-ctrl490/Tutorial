b539501b9480fdcb79f843e265add76515ef5965
Parth Shukla <2k23.psitaiml2310215@gmail.com>
2025-04-07 22:24:36 +0530

    Create main.java

---
+++ main.java
@@ -0,0 +1,7 @@
+public class Main{
+    public static void main(String[] args){
+        Encapsulation obj=new Encapsulation();
+        obj.setname("parth");
+        System.out.println(obj.getname());
+    }
+}
